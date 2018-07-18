1eff1d9758684e4f6ab5a0db713d9b0f47d9498b
michwii <freezerhm@yahoo.fr>
2018-07-18 11:05:27 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+var request = require('request');
+var async = require('async');
+var commons = require("../shared/commons");
+var console = {};
+
+var subscriptionId = process.env.SUBSCRIPTION_ID ;
+var tenant_id = process.env.TENANT_ID;
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
+var resource_group_exclusions = process.env.RESOURCE_GROUP_EXCLUSIONS.split(',').map(function(x){ return x.toUpperCase();});
+var delay_before_destruction = process.env.DELAY_BEFORE_DESTRUCTION;
+
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    console = context;
+
+    var deadline = new Date();
+    deadline.setTime(deadline.getTime() - (parseInt(delay_before_destruction)*1000));
+
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    console.log("Resource groups that are going to be ignored : ");
+    console.log(resource_group_exclusions);   
+    console.log("For the others, they will be deleted if they were created before : ");
+    console.log(deadline);  
+    console.log("Starting to analyse...");
+    await deleteUnUsedResourceGroups();
+    console.log("Work completed...");
+    context.done();
+};
+
+
+var deleteUnUsedResourceGroups = async function (){
+    var accessToken = await commons.getAccessToken(tenant_id, client_id, client_secret);
+    var resourceGroups = await getResourceGroupList(subscriptionId, accessToken);
+    var resourceGroupNamesToDelete = new Array();
+    for(var resourceGroup of resourceGroups){
+        var deploymentsHistory = await getDeploymentsHistoryByResourceGroup(subscriptionId, accessToken, resourceGroup.name);
+        var iCanDestroy = canIToDestroyThisResourceGroup(deploymentsHistory);
+        var resourceGroupShouldBeExcluded = resource_group_exclusions.indexOf(resourceGroup.name.toUpperCase()) !== -1;
+        if(iCanDestroy && !resourceGroupShouldBeExcluded){
+            resourceGroupNamesToDelete.push(resourceGroup.name);
+            console.log("Ressource group supprimé : " + resourceGroup.name);
+        }
+    }
+    var resourceGroupsToDeletePromised = resourceGroupNamesToDelete.map(deleteResourceGroup.bind(null, subscriptionId, accessToken));
+    Promise.all(resourceGroupsToDeletePromised);
+}
+
+var getResourceGroupList = async function(subscriptionId, accessToken){
+    var url = "https://management.azure.com/subscriptions/"+ subscriptionId +"/resourcegroups?api-version=2017-05-10";
+    return commons.getDataFromMSAPI(accessToken, url) ;
+}
+
+var getDeploymentsHistoryByResourceGroup = async function(subscriptionId, accessToken, resourceGroupName) {
+    var url = "https://management.azure.com/subscriptions/" + subscriptionId + "/resourcegroups/" + resourceGroupName + "/providers/Microsoft.Resources/deployments/?api-version=2018-02-01";
+    return commons.getDataFromMSAPI(accessToken, url) ;
+}
+
+var canIToDestroyThisResourceGroup = function(deploymentsHistory, exlusions, delayBeforeDestroying) {
+    var latestDeployment = getLastDeployment(deploymentsHistory);
+    var deadline = new Date();
+    deadline.setTime(deadline.getTime() - (parseInt(delay_before_destruction)*1000));
+    return deadline >= latestDeployment;
+}
+
+var getLastDeployment = function(deploymentsHistory){
+    var lastDate = new Date();
+    lastDate.setTime(0);
+    for(var deployment of deploymentsHistory){
+        var deploymentDate = new Date(deployment.properties.timestamp);
+        if(deploymentDate > lastDate){
+            lastDate = deploymentDate;
+        }
+    }
+    return lastDate;
+}
+
+var deleteResourceGroup = async function(subscriptionId, accessToken, resourceGroupName){
+    var url = "https://management.azure.com/subscriptions/"+subscriptionId+"/resourcegroups/" + resourceGroupName + "?api-version=2018-02-01";
+    return commons.getDataFromMSAPI(accessToken, url, "DELETE") ;
+}
