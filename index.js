6f2f1408c3e6afd01461f08dac5fdf836c16614e
michwii <freezerhm@yahoo.fr>
2025-06-04 09:56:05 +0200

    Refactor function to use axios

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var request = require('request');
-var console = {};
+'use strict';
+const axios = require('axios');
+let logger = console;
 
 /**
     Retrieving all the application settings that are mandatory. 
     You should create a client principal in your azure subscription that have the right to READ / UPDATE and DELETE resources in Azure.
-    list of resource groups to exlude separated by a comma. Not case sensitive. You should put at least the resource group where your Godzilla stands.
+    list of resource groups to exclude separated by a comma. Not case sensitive. You should put at least the resource group where your Godzilla stands.
     Value of the parameter delay_before_destruction should be set in seconds.
 */
-var subscriptionId = process.env.SUBSCRIPTION_ID ;
-var tenant_id = process.env.TENANT_ID;
-var client_id = process.env.CLIENT_ID;
-var client_secret = process.env.CLIENT_SECRET;
-var resource_group_exclusions = process.env.RESOURCE_GROUP_EXCLUSIONS.split(',').map(function(x){ return x.toUpperCase();});
-var delay_before_destruction = process.env.DELAY_BEFORE_DESTRUCTION;
+const subscriptionId = process.env.SUBSCRIPTION_ID;
+const tenantId = process.env.TENANT_ID;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const resource_group_exclusions = (process.env.RESOURCE_GROUP_EXCLUSIONS || '').split(',').map(x => x.toUpperCase());
+const delay_before_destruction = process.env.DELAY_BEFORE_DESTRUCTION;
 
-module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    console = context;
+module.exports = async (context, myTimer) => {
+    logger = context;
 
-    var deadline = new Date();
+    const deadline = new Date();
     deadline.setTime(deadline.getTime() - (parseInt(delay_before_destruction)*1000));
 
     if(myTimer.isPastDue)
@@ -27,22 +27,22 @@ module.exports = async function (context, myTimer) {
     }
 
     if(allRequirementsArePresent()){
-        console.log("Resource groups that are going to be ignored : ");
-        console.log(resource_group_exclusions);   
-        console.log("For the others, they will be deleted if they were created before : ");
-        console.log(deadline);  
-        console.log("Starting to analyse...");
+        logger.log('Resource groups that are going to be ignored : ');
+        logger.log(resource_group_exclusions);
+        logger.log('For the others, they will be deleted if they were created before : ');
+        logger.log(deadline);
+        logger.log('Starting to analyse...');
         await deleteUnUsedResourceGroups();
-        console.log("Work completed...");
+        logger.log('Work completed...');
         context.done();
     }else{
-        console.log("Sorry but not all the required environment variables have been set...");
-        console.log("To work properly, this application needs to be set in the application settings the following parameters : ");
-        console.log("TENANT_ID");
-        console.log("SUBSCRIPTION_ID");
-        console.log("DELAY_BEFORE_DESTRUCTION");
-        console.log("CLIENT_ID");
-        console.log("CLIENT_SECRET");
+        logger.log('Sorry but not all the required environment variables have been set...');
+        logger.log('To work properly, this application needs to set the following parameters :');
+        logger.log('TENANT_ID');
+        logger.log('SUBSCRIPTION_ID');
+        logger.log('DELAY_BEFORE_DESTRUCTION');
+        logger.log('CLIENT_ID');
+        logger.log('CLIENT_SECRET');
         context.done();
     }
 
@@ -52,117 +52,105 @@ module.exports = async function (context, myTimer) {
    Making sure all the Application Settings are set and not empty 
    @return a boolean wheter or not the application can start.
 */
-var allRequirementsArePresent = function(){
-    return subscriptionId && tenant_id && client_id && client_secret && resource_group_exclusions && delay_before_destruction ;
-}
+const allRequirementsArePresent = () => {
+    return subscriptionId && tenantId && clientId && clientSecret && resource_group_exclusions && delay_before_destruction;
+};
 
-var deleteUnUsedResourceGroups = async function (){
-    var accessToken = await getAccessToken(tenant_id, client_id, client_secret);
-    var resourceGroups = await getResourceGroupList(subscriptionId, accessToken);
-    var resourceGroupNamesToDelete = new Array();
-    for(var resourceGroup of resourceGroups){
-        var deploymentsHistory = await getDeploymentsHistoryByResourceGroup(subscriptionId, accessToken, resourceGroup.name);
-        var iCanDestroy = canIToDestroyThisResourceGroup(deploymentsHistory);
-        var resourceGroupShouldBeExcluded = resource_group_exclusions.indexOf(resourceGroup.name.toUpperCase()) !== -1;
-        if(iCanDestroy && !resourceGroupShouldBeExcluded){
+const deleteUnUsedResourceGroups = async () => {
+    const accessToken = await getAccessToken(tenantId, clientId, clientSecret);
+    const resourceGroups = await getResourceGroupList(subscriptionId, accessToken);
+    const resourceGroupNamesToDelete = [];
+    for (const resourceGroup of resourceGroups) {
+        const deploymentsHistory = await getDeploymentsHistoryByResourceGroup(subscriptionId, accessToken, resourceGroup.name);
+        const iCanDestroy = canIToDestroyThisResourceGroup(deploymentsHistory);
+        const resourceGroupShouldBeExcluded = resource_group_exclusions.indexOf(resourceGroup.name.toUpperCase()) !== -1;
+        if (iCanDestroy && !resourceGroupShouldBeExcluded) {
             resourceGroupNamesToDelete.push(resourceGroup.name);
-            console.log("Ressource group supprimé : " + resourceGroup.name);
+            logger.log('Resource group deleted: ' + resourceGroup.name);
         }
     }
-    var resourceGroupsToDeletePromised = resourceGroupNamesToDelete.map(deleteResourceGroup.bind(null, subscriptionId, accessToken));
-    Promise.all(resourceGroupsToDeletePromised);
-}
+    const resourceGroupsToDeletePromised = resourceGroupNamesToDelete.map(deleteResourceGroup.bind(null, subscriptionId, accessToken));
+    await Promise.all(resourceGroupsToDeletePromised);
+};
 
 /**
-    @return Array of Ressource group 
+    @return Array of Resource groups
 */
-var getResourceGroupList = async function(subscriptionId, accessToken){
-    var url = "https://management.azure.com/subscriptions/"+ subscriptionId +"/resourcegroups?api-version=2017-05-10";
-    return getDataFromMSAPI(accessToken, url) ;
-}
+const getResourceGroupList = async (subscriptionId, accessToken) => {
+    const url = `https://management.azure.com/subscriptions/${subscriptionId}/resourcegroups?api-version=2017-05-10`;
+    return getDataFromMSAPI(accessToken, url);
+};
 
 /**
     @return Array of Deployment History for a specific resource group
 */
-var getDeploymentsHistoryByResourceGroup = async function(subscriptionId, accessToken, resourceGroupName) {
-    var url = "https://management.azure.com/subscriptions/" + subscriptionId + "/resourcegroups/" + resourceGroupName + "/providers/Microsoft.Resources/deployments/?api-version=2018-02-01";
-    return getDataFromMSAPI(accessToken, url) ;
-}
+const getDeploymentsHistoryByResourceGroup = async (subscriptionId, accessToken, resourceGroupName) => {
+    const url = `https://management.azure.com/subscriptions/${subscriptionId}/resourcegroups/${resourceGroupName}/providers/Microsoft.Resources/deployments/?api-version=2018-02-01`;
+    return getDataFromMSAPI(accessToken, url);
+};
 
 /**
     @return a boolean Wheter or not we can delete the resource group.
 */
-var canIToDestroyThisResourceGroup = function(deploymentsHistory, exlusions, delayBeforeDestroying) {
-    var latestDeployment = getLastDeployment(deploymentsHistory);
-    var deadline = new Date();
-    deadline.setTime(deadline.getTime() - (parseInt(delay_before_destruction)*1000));
+const canIToDestroyThisResourceGroup = (deploymentsHistory) => {
+    const latestDeployment = getLastDeployment(deploymentsHistory);
+    const deadline = new Date();
+    deadline.setTime(deadline.getTime() - (parseInt(delay_before_destruction) * 1000));
     return deadline >= latestDeployment;
-}
+};
 
 /**
     @return the latest deployement date of a resource group.
     For empty resource groups or resource group without deployment (deploying a storage account is not considered as a deployment),
     current timestamp in date format is returned.
 */
-var getLastDeployment = function(deploymentsHistory){
-    var lastDate = new Date();
-    lastDate.setTime(0);
-    for(var deployment of deploymentsHistory){
-        var deploymentDate = new Date(deployment.properties.timestamp);
-        if(deploymentDate > lastDate){
+const getLastDeployment = (deploymentsHistory) => {
+    let lastDate = new Date(0);
+    for (const deployment of deploymentsHistory) {
+        const deploymentDate = new Date(deployment.properties.timestamp);
+        if (deploymentDate > lastDate) {
             lastDate = deploymentDate;
         }
     }
     return lastDate;
-}
+};
 
-var deleteResourceGroup = async function(subscriptionId, accessToken, resourceGroupName){
-    var url = "https://management.azure.com/subscriptions/"+subscriptionId+"/resourcegroups/" + resourceGroupName + "?api-version=2018-02-01";
-    return getDataFromMSAPI(accessToken, url, "DELETE") ;
-}
+const deleteResourceGroup = async (subscriptionId, accessToken, resourceGroupName) => {
+    const url = `https://management.azure.com/subscriptions/${subscriptionId}/resourcegroups/${resourceGroupName}?api-version=2018-02-01`;
+    return getDataFromMSAPI(accessToken, url, 'DELETE');
+};
 
 /**
     return accessToken that can be used to perform action on Azure REST API.
 */
-var getAccessToken = async function(tenant_id, client_id, client_secret){
-    return new Promise((resolved, rejected) => {
-        var url = "https://login.microsoftonline.com/" + tenant_id + "/OAuth2/Token";
-        request.post({url:url, form: {
-            grant_type : "client_credentials",
-            client_id : client_id,
-            client_secret : client_secret,
-            resource : "https://management.core.windows.net/"
-        }}, function(err, httpResponse, body){
-            if(err){
-                rejected(err);
-            }else{
-                var accessToken = JSON.parse(body).access_token;
-                resolved(accessToken);
-            }
+const getAccessToken = async (tenantId, clientId, clientSecret) => {
+    const url = `https://login.microsoftonline.com/${tenantId}/OAuth2/Token`;
+    try {
+        const response = await axios.post(url, {
+            grant_type: 'client_credentials',
+            client_id: clientId,
+            client_secret: clientSecret,
+            resource: 'https://management.core.windows.net/'
         });
-    });
+        return response.data.access_token;
+    } catch (err) {
+        throw err;
+    }
 };
 
 /**
     Helper that getting result from MS API. It abstract the use of Request librairy.
     @return MS API request result.
 */
-var getDataFromMSAPI = async function(accessToken, url, method){
-    return new Promise((resolved, rejected) => {
-        method = (method) ? method : 'GET';
-        request({
-            url : url,
-            method : method,
-            'auth': {
-                'bearer': accessToken
-            }
-        }, function(err, httpResponse, body){
-            if(err){
-                rejected(err);
-            }else{
-                var dataToReturn = JSON.parse(body).value;
-                resolved(dataToReturn);
-            }
+const getDataFromMSAPI = async (accessToken, url, method = 'GET') => {
+    try {
+        const response = await axios({
+            url,
+            method,
+            headers: { Authorization: `Bearer ${accessToken}` }
         });
-    });
-}
+        return response.data.value;
+    } catch (err) {
+        throw err;
+    }
+};
